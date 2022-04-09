870205a9d045f8b919aa80257732f93a370b5a51
Zachary-collins7 <Zachary.collins1231@gmail.com>
2022-04-09 01:59:00 -0600

    push

--- src/js/app.js
+++ src/js/app.js
@@ -17,12 +17,12 @@ const demo = () => moment("20111031", "YYYYMMDD").fromNow();
 
 barba.init({
     views: [{
-        namespace: 'index'
+        namespace: "index"
     }, {
-        namespace: 'contact'
+        namespace: "contact"
     }],
     transitions: [{
-        name: 'opacity-transition',
+        name: "opacity-transition",
         leave(data) {
             return gsap.to(data.current.container, {
                 opacity: 0
@@ -36,7 +36,34 @@ barba.init({
     }]
 });
 
+function createFloatingNav() {
+    var ul = $("<ul>").append(
+        $(".full-page-section").map(function () {
+            return $("<li>").append(
+                $("<a>", {
+                    href: "#" + $(this).attr("data-section-name")
+                })
+            )[0]
+        })
+    )
+    $("<div>", {
+        id: "f-nav",
+        class: "f-nav",
+    })
+        .append(ul)
+        .appendTo('body');
+}
+
+function onScrollSectionChange() {
+    var sectionTag = $.scrollify.current().attr("data-section-name")//window.location.hash.slice(1);
+    $(".f-nav ul li a").each(function() {
+        $(this).removeClass("active");
+    })
+    $(`.f-nav ul li a[href$=${sectionTag}]`).addClass("active")
+}
+
 $(function () {
+    createFloatingNav();
     $.scrollify({
         section: ".full-page-section",
         easing: "easeOutExpo",
@@ -48,20 +75,11 @@ $(function () {
         overflowScroll: true,
         updateHash: true,
         touchScroll: true,
-        before: function () { },
+        before: function () { onScrollSectionChange() },
         after: function () { },
         afterResize: function () { },
-        afterRender: function () { }
+        afterRender: function () { onScrollSectionChange() }
     });
-
-    // floating Nav
-    let fNav = $('<div>', {
-        id: 'f-nav',
-        class: 'nav',
-    }).appendTo('body');
-
-    $('.section').each(function() {
-        
-    })
 });
+
 
