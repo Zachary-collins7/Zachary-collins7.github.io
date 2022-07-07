bd8fd3fedd9f4fc3fdc133c3a32623ee050da597
Zachary-collins7 <Zachary.collins1231@gmail.com>
2022-07-07 03:53:11 -0600

    spacing

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+
 echo -e "\033[0;36minfo\033[0m  - Removing .DS_Store files if any"
 rm -rf portfolio/out/.DS_Store;
+
 
 echo -e "\033[0;36minfo\033[0m  - adding .DS_Store file to .gitignore"
 echo ".DS_Store" > portfolio/out/.gitignore;
 
+
 echo -e "\033[0;36minfo\033[0m  - Creating \033[0;32m .nojekyll\033[0m file in out folder "
 touch portfolio/out/.nojekyll;
+
 
 echo -e "\033[0;36minfo\033[0m  - Copying .env's to out folder "
 cp portfolio/.env portfolio/out/.env;
@@ -16,6 +20,7 @@ cp portfolio/.env.production portfolio/out/.env.production;
 
 echo -e "\033[0;36minfo\033[0m  - Delete old gh-pages branch"
 git branch -D gh-pages
+
 
 echo -e "\033[0;36minfo\033[0m  - Moving\033[0;32m built application\033[0m to gh-pages branch "
 git checkout --orphan gh-pages
