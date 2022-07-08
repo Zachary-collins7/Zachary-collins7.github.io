26e24f365e4318d551c5b4b133e17d6efa73bdd5
Zachary-collins7 <Zachary.collins1231@gmail.com>
2022-07-08 10:10:51 -0600

    didn't work as expected

--- portfolio/next.config.js
+++ portfolio/next.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: process.env.NODE_ENV === 'production' ? 'd1b6q6iv3yqcky.cloudfront.net' : '',
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'app/styles')],
