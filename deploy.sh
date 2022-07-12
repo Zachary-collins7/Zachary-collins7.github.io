b0c1caf28137e41d4934dd0c0ffa12c441846e82
Zachary-collins7 <Zachary.collins1231@gmail.com>
2022-07-12 11:46:21 -0600

    added sleeps to fix potential bug

--- deploy.sh
+++ deploy.sh
@@ -7,24 +7,25 @@ rm -rf portfolio/out/.DS_Store;
 
 echo -e "\033[0;36minfo\033[0m  - adding .DS_Store file to .gitignore"
 echo ".DS_Store" > portfolio/out/.gitignore;
-
+sleep 1;
 
 echo -e "\033[0;36minfo\033[0m  - Creating \033[0;32m .nojekyll\033[0m file in out folder "
 touch portfolio/out/.nojekyll;
+sleep 1;
 
 
 echo -e "\033[0;36minfo\033[0m  - Copying .env's to out folder "
 \cp portfolio/.env portfolio/out/.env;
-\cp portfolio/.env.production portfolio/out/.env.production;
+\cp portfolio/.env.production portfolio/out/.env.production; 
+sleep 1;
 
 
 echo -e "\033[0;36minfo\033[0m  - Delete old gh-pages branch"
-git branch -D gh-pages
-
+git branch -D gh-pages; sleep 1;
 
 echo -e "\033[0;36minfo\033[0m  - Moving\033[0;32m built application\033[0m to gh-pages branch "
-git checkout --orphan gh-pages
-git rm -r --cached .
-git --work-tree portfolio/out add --all
-git --work-tree portfolio/out commit -m 'deploy gh-pages'
+git checkout --orphan gh-pages; sleep 1;
+git rm -r --cached .; sleep 1;
+git --work-tree portfolio/out add --all; sleep 1;
+git --work-tree portfolio/out commit -m 'deploy gh-pages'; sleep 1;
 git push origin HEAD:gh-pages --force && git checkout -f main
