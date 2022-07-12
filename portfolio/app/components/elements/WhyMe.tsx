c2e639e1491751314f1c075b8a39ddd820b8f2e8
Zachary-collins7 <Zachary.collins1231@gmail.com>
2022-07-12 13:29:36 -0600

    added thumbnails to videos

--- portfolio/app/components/elements/WhyMe.tsx
+++ portfolio/app/components/elements/WhyMe.tsx
@@ -10,22 +10,26 @@ export default function WhyMe(props: IWhyMeProps) {
         <>
             <div className={styles.wrapper}>
                 <Card title={"Responsive Design"} text={"My layouts will work on any device, big or small"} className={styles.responsive}>
-                    <video autoPlay loop muted playsInline style={{
-                        position: "absolute",
-                        top: "0",
-                        left: "0",
-                        width: "100%",
-                        height: "100%",
-                        objectFit: "cover",
-                    }}>
+                    <video autoPlay loop muted playsInline
+                        poster="/assets/responsiveAnimation.webp"
+                        style={{
+                            position: "absolute",
+                            top: "0",
+                            left: "0",
+                            width: "100%",
+                            height: "100%",
+                            objectFit: "cover",
+                        }}>
                         <source src="/assets/responsiveAnimation.webm" type="video/webm" />
                         <source src="/assets/responsiveAnimation.mp4" type="video/mp4" />
+                        Sorry, your browser doesn't support embedded videos.
                     </video>
                 </Card>
 
                 <Card title={"Modern Development"} className={styles.modern}>
                     <p>I integrate modern frameworks like <b>ReactJS</b> and <b>NextJS</b>.</p>
                     <p>I'll update your website regularly to keep it up to date with the <b>latest trends</b> and technologies.</p>
+                    <p></p>
                 </Card>
 
                 <Card title={"Search Engine Optimization"} className={styles.seo}>
@@ -33,16 +37,19 @@ export default function WhyMe(props: IWhyMeProps) {
                 </Card>
 
                 <Card title={"Really Fast Loadtimes"} text={"Fast load times and lag free interaction, my highest priority"} className={styles.performance}>
-                    <video autoPlay loop muted playsInline style={{ 
-                        position: "absolute",
-                        top: "0",
-                        left: "0",
-                        width: "100%",
-                        height: "100%",
-                        objectFit: "cover",
-                     }}>
-                        <source src="/assets/rocket.webm" type="video/webm" />
-                        <source src="/assets/rocket.mp4" type="video/mp4" />
+                    <video autoPlay loop muted playsInline
+                        poster="/assets/rocketAnimation.webp"
+                        style={{
+                            position: "absolute",
+                            top: "0",
+                            left: "0",
+                            width: "100%",
+                            height: "100%",
+                            objectFit: "cover",
+                        }}>
+                        <source src="/assets/rocketAnimation.webm" type="video/webm" />
+                        <source src="/assets/rocketAnimation.mp4" type="video/mp4" />
+                        Sorry, your browser doesn't support embedded videos.
                     </video>
                 </Card>
 
