4607442c5b388a4a2869549366ec2e8b81c8bd81
Zachary-collins7 <Zachary.collins1231@gmail.com>
2022-07-24 15:10:32 -0600

    Update next-seo.config.ts

--- portfolio/next-seo.config.ts
+++ portfolio/next-seo.config.ts
@@ -41,21 +41,3 @@ const SEO: import('next-seo').DefaultSeoProps = {
     }
 }
 export default SEO;
-
-
-// defaultTitle: "Zachary's Portforlio",
-//     titleTemplate: "%s | Zachary's Portforlio",
-//         openGraph: {
-//     type: 'website',
-//         url: process.env.SITE_URL || 'example.com',
-//             site_name: "Zachary's Portforlio",
-//                 profile: {
-//         firstName: 'Zachary',
-//             lastName: 'Collins',
-//         },
-// },
-// twitter: {
-//     handle: '@ZachTheCollins',
-//         site: '@ZachTheCollins',
-//             cardType: 'summary_large_image',
-//     }
