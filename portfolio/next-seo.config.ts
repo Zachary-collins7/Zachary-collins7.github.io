330b62f7f86a68bbebd6974af36fd80af481b79c
Zachary-collins7 <Zachary.collins1231@gmail.com>
2022-07-24 15:18:19 -0600

    Update next-seo.config.ts

--- portfolio/next-seo.config.ts
+++ portfolio/next-seo.config.ts
@@ -4,7 +4,7 @@ const SEO: import('next-seo').DefaultSeoProps = {
     openGraph: {
         type: 'website',
         locale: 'en_US',
-        url: process.env.SITE_URL || "",
+        url: process.env.SITE_URL || 'http://zachary-collins7.github.io',
         site_name: "Zachary's Portfolio",
         profile: {
             firstName: 'Zachary',
@@ -12,21 +12,21 @@ const SEO: import('next-seo').DefaultSeoProps = {
         },
         images: [
             {
-                url: "/assets/images/portfolioScreenshot1.webp",
+                url: '/assets/images/portfolioScreenshot1.webp',
                 width: 1200,
                 height: 630,
                 alt: 'Portfolio Site Screenshot',
                 type: 'image/webp'
             },
             {
-                url: "/assets/images/portfolioScreenshot1.jpg",
+                url: '/assets/images/portfolioScreenshot1.jpg',
                 width: 1200,
                 height: 630,
                 alt: 'Portfolio Site Screenshot',
                 type: 'image/jpeg'
             },
             {
-                url: "/assets/images/portfolioScreenshot1.png",
+                url: '/assets/images/portfolioScreenshot1.png',
                 width: 1200,
                 height: 630,
                 alt: 'Portfolio Site Screenshot',
@@ -38,6 +38,12 @@ const SEO: import('next-seo').DefaultSeoProps = {
         cardType: 'summary_large_image',
         site: '@ZachTheCollins',
         handle: '@ZachTheCollins'   
-    }
+    },
+    additionalMetaTags: [
+        {
+            property: 'twitter:image',
+            content: '/assets/images/portfolioScreenshot1.webp'
+        }
+    ]
 }
 export default SEO;
