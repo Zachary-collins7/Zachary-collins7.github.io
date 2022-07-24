3744ba48cfd5916c7f1450e3652022ed0e425f53
Zachary-collins7 <Zachary.collins1231@gmail.com>
2022-07-24 15:31:55 -0600

    facebook's tool is way better at debugging meta tags

--- portfolio/next-seo.config.ts
+++ portfolio/next-seo.config.ts
@@ -1,10 +1,13 @@
+const fallBackSiteUrl = 'http://zachary-collins7.github.io'
+const siteUrl = process.env.SITE_URL || fallBackSiteUrl
+
 const SEO: import('next-seo').DefaultSeoProps = {
     defaultTitle: "Zachary's Portfolio",
     titleTemplate: "%s | Zachary's Portfolio",
     openGraph: {
         type: 'website',
         locale: 'en_US',
-        url: process.env.SITE_URL || 'http://zachary-collins7.github.io',
+        url: siteUrl,
         site_name: "Zachary's Portfolio",
         profile: {
             firstName: 'Zachary',
@@ -12,21 +15,21 @@ const SEO: import('next-seo').DefaultSeoProps = {
         },
         images: [
             {
-                url: '/assets/images/portfolioScreenshot1.webp',
+                url: siteUrl + '/assets/images/portfolioScreenshot1.webp',
                 width: 1200,
                 height: 630,
                 alt: 'Portfolio Site Screenshot',
                 type: 'image/webp'
             },
             {
-                url: '/assets/images/portfolioScreenshot1.jpg',
+                url: siteUrl + '/assets/images/portfolioScreenshot1.jpg',
                 width: 1200,
                 height: 630,
                 alt: 'Portfolio Site Screenshot',
                 type: 'image/jpeg'
             },
             {
-                url: '/assets/images/portfolioScreenshot1.png',
+                url: siteUrl + '/assets/images/portfolioScreenshot1.png',
                 width: 1200,
                 height: 630,
                 alt: 'Portfolio Site Screenshot',
@@ -41,12 +44,6 @@ const SEO: import('next-seo').DefaultSeoProps = {
     },
     facebook: {
         appId: '5348420825250671',
-    },
-    additionalMetaTags: [
-        {
-            property: 'twitter:image',
-            content: '/assets/images/portfolioScreenshot1.webp'
-        }
-    ]
+    }
 }
 export default SEO;
