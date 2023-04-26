0b2764e945bd7a3a994e787d57e46e67ebec7f44
Zachary-collins7 <Zachary.collins1231@gmail.com>
2023-04-26 15:49:05 -0600

    updated resume

--- portfolio/pages/resume.tsx
+++ portfolio/pages/resume.tsx
@@ -1,4 +1,4 @@
-import React, { useId } from "react";
+import React, { useEffect, useId, useState } from "react";
 import type { GetStaticProps, InferGetStaticPropsType } from 'next';
 import styles from "./resume.module.scss";
 import { NextSeo } from "next-seo";
@@ -12,22 +12,33 @@ export const getStaticProps: GetStaticProps<staticProps> = async (context) => {
     return {
         props: {
             updatedAt
-        }, // will be passed to the page component as props
+        }, // will be passed to the page component as props on compile
     }
 }
 
 const Resume = ({ updatedAt }: InferGetStaticPropsType<typeof getStaticProps>) => {
+    // get phone number and email from the url arguments (if they exist)
+    const [phone, setPhone] = useState("XXX-XXX-XXXX");
+    useEffect(() => {
+        const url = new URL(window.location.href);
+        const phone_raw = url.searchParams.get("phone") || "XXXXXXXXXX";
+        const phone_formatted = phone_raw.replace(/(\d{3})(\d{3})(\d{4})/, "$1-$2-$3");
+        setPhone(
+            phone_formatted
+        );
+    }, []);
+    
     return (
         <>
             <NextSeo title="Resume" description="Web version of my Resume"/>
             <div className={styles.wrapper}>
                 <div className={styles.footer}>
-                    <a
+                    {/* <a
                         href="https://github.com/Zachary-collins7/Zachary-collins7/blob/main/resume2022.pdf"
                         target="_blank"
                         rel="noopener noreferrer">
-                        Download pdf version here
-                    </a>
+                        Download pdf
+                    </a> */}
                     
                     <span>
                         Last updated: {updatedAt}
@@ -38,7 +49,11 @@ const Resume = ({ updatedAt }: InferGetStaticPropsType<typeof getStaticProps>) =
 
                     <div className={styles.about}>
                         <p>
-                            <i>Developer with 7 years of Programming experience.</i><br />
+                            <i>
+                                Developer with 7 years of Programming experience and a passion for creating
+                            </i>
+                            
+                            <br />
 
                             Portfolio: <a
                                 href="http://zachary-collins7.github.io"
@@ -65,29 +80,58 @@ const Resume = ({ updatedAt }: InferGetStaticPropsType<typeof getStaticProps>) =
                             <ul>
                                 <li><b>Programming Languages:</b> Python | Typescript/Javascript | Swift | PHP</li>
                                 <li><b>Framework/Platforms:</b> ReactJS | NextJS | Laravel | SwiftUI | .NET</li>
-                                <li><b>Databases:</b> MySQL | MongoDB |  MariaDB | Sqlite3</li>
+                                <li><b>Databases:</b> MySQL | MongoDB | MariaDB | Sqlite3</li>
                             </ul>
                         </div>
                         <div className={styles.skillSpacer}></div>
-                        {/* <div className={styles.skillsRight}>
+                        <div className={styles.skillsRight}>
                             <ul>
                                 <li>
                                     <a href="mailto:zachary_collins@icloud.com">zachary_collins@icloud.com</a>
                                 </li>
                                 <li>
-                                    <a href="tel:+1-918-574-4653">+X (XXX) XXX XXXX</a>
+                                    <a href="tel:+1-918-574-4653">+1 {phone}</a>
                                 </li>
                                 <li>
                                     Durango CO, USA
                                 </li>
                             </ul>
-                        </div> */}
+                        </div>
                     </div>
                 </section>
 
                 <section className={styles.section}>
                     <h2 className={styles.title}>Personal Projects</h2>
-
+                    <Project 
+                        title="Discord Bot"
+                        date="Feb 2023"
+                        tools={["Python", "GPT-3", "Selenium", "Google Translate", "Discord-api", "Mysql"]}>
+                        <ul>
+                            <li>
+                                Created a discord bot to connect users of a game to resources and information
+                                provided by the game and the community. <br/> With over 14,000 daily users, I focused on
+                                making the bot as efficient and fast as possible while still providing a great user experience.
+                            </li>
+                            <li>
+                                The bot uses Selenium to scrape the game&apos;s website and uses Google Translate and GPT-3 to translate the
+                                information into the user&apos;s language. <br/> Translated information is cached in a database to prevent
+                                unnecessary requests.
+                            </li>
+                            <li>
+                                Using information provided by the users, the bot calculates insights about the individual user&apos;s progress
+                                and provides them with personalized information. Personalized information is also translated into the user&aposs 
+                                language (if supported). The information is also used to hit the game&aposs API to give the user in-game items.
+                            </li>
+                            <li>
+                                Detailed logs are kept for each user to help with debugging and to provide insight into how the bot is being used.
+                                Logs are queried, sorted, and displayed in a dashboard for easy viewing. <br/> 
+                                <i>
+                                    A Terms of Service and Privacy Policy are
+                                    also provided to all users to comply with Discord&apos;s terms of service.
+                                </i>
+                            </li>
+                        </ul>
+                    </Project>
                     <Project
                         title="Portfolio Website"
                         date="May 2022"
@@ -97,7 +141,7 @@ const Resume = ({ updatedAt }: InferGetStaticPropsType<typeof getStaticProps>) =
                                 Deployed using NextJS static site generation to Github Pages (uses a work-tree deployment branch alongside the main branch)
                             </li>
                             <li>
-                                Site features a fully decked-out SEO (including sitemap and robots.txt), color themes, Google Analytics, and a custom 404 page.
+                                The Site features a fully decked-out SEO (including sitemap and robots.txt), color themes, Google Analytics, and a custom 404 page.
                             </li>
                             <li>
                                 Uses SCSS modules (non-decorated) to prevent CSS class-name collisions.
@@ -114,25 +158,27 @@ const Resume = ({ updatedAt }: InferGetStaticPropsType<typeof getStaticProps>) =
 
                     <Project
                         title="Comcast – Python Developer III - Contract"
-                        date="Aug 2022 – Oct 2022"
+                        date="Aug 2022 – Apr 2023"
                         location="Durango, CO"
                         tools={["Python", "EC2", "Mysql", "MongoDB", "S3", "Oracle", 
-                        "Postgresql", "DynamoDB", "Cassandra", "Memsql", "Teradata", "Redis"]}>
+                        "Postgresql", "DynamoDB", "Teradata", "Redis"]}>
                         <ul>
                             <li>
-                                Created a python project to create test connections to 10,000+ database nodes of various database types
-                                (including: Mysql, Oracle, MongoDB, Postgresql) and report the results to a database. <br/>
-                                - Collated the database nodes by host-name to prevent connecting to the same host multiple times. <br/>
+                                Developed a python-based project to test connections to 10,000+ database nodes of various types,
+                                resulting in comprehensive analytics about database functionality, user access, and credential verification
+                            </li>
+                            <li>
+                                Developed a project focused on gathering metadata, samples, and analytics for data auditors.
+                                The samples and analytics were used to give insight to the data auditors about the data they
+                                were auditing - a process that was previously done manually. <br/>
+                                Profiled Data was delivered to a web application for users to view and interact with.
+                            </li>
+                            <li>
+                                For both projects, I architected the following: <br/>
+                                - Collated the database nodes by hostname to prevent connecting to the same host multiple times. <br/>
                                 - Utilized python libraries to multi-thread the connections to increase the speed of the test. <br/>
-                                - Results were used to determine if a database was up and running, identify the user&apos;s query access across the schema,
-                                and check if the internal password manager contained the correct credentials.
-                            </li>
-                            <li>
-                                Converted the project to a pip installable package to promote reusability and ease of use across other projects.
-                            </li>
-                            <li>
-                                Connection Data was delivered to a web application for users to view.
-                                Users could flag columns to be profiled and the system would preform a set of queries to profile the specific flagged columns. 
+                                - Provided reporting of the results to a database and to my team. <br/>
+                                - Converted the project to a pip installable package to promote reusability and ease of use across other projects. <br/>
                             </li>
                         </ul>
                     </Project>
