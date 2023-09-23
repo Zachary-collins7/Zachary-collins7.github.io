374cb9aa70fb830aec0dcbd2388497a56d30dbed
Zachary-collins7 <Zachary.collins1231@gmail.com>
2023-09-22 18:32:09 -0600

    hero and datastructure module for animation

--- portfolio/src/components/hero.tsx
+++ portfolio/src/components/hero.tsx
@@ -1,101 +1,184 @@
 "use client";
 import { useEffect, useState } from "react";
+import {
+    levenshteinDistance,
+    LevenshteinDistanceResult,
+} from "../util/dataStructures"; //"@util/dataStructures";
 import styles from "./hero.module.scss";
+import Button from "./ui/button";
 
 export default function Hero() {
     return (
         <div className={styles.wrapper}>
             <div className={styles.left}>
-                <Greeting varient={1} />
+                <div>
+                    <Greeting />
+                </div>
             </div>
-            {/* <div className={styles.right}>world</div> */}
+            <div className={styles.right}>
+                <div></div>
+            </div>
         </div>
     );
 }
 
-const Greeting = ({ varient }: { varient: number }) => {
+const Greeting = () => {
     const allTitles = [
         "Software Engineer",
         "Fullstack Developer",
         "Prompt Engineer",
-        // "Dev",
     ];
-    const titleChangeDelayMs = 10000;
-    const [titleIndex, setTitleIndex] = useState(0);
-    const [title, setTitle] = useState(allTitles[titleIndex]);
+    const titleChangeDelayMs = 5000;
+    const [titleIndex, setTitleIndex] = useState(1); // TODO: check prod. no idea why this is 1 and not 0
+    const [title, setTitle] = useState(allTitles[0]);
+    const [wordChanges, setWordChanges]: [LevenshteinDistanceResult[], any] =
+        useState([]);
 
     // every 5 seconds change title index
     useEffect(() => {
-        const interval = setInterval(() => {
-            setTitleIndex((titleIndex + 1) % allTitles.length);
+        const interval = setInterval(async () => {
+            const newIndex = (titleIndex + 1) % allTitles.length;
+            const oldTitle = title;
+            const newTitle = allTitles[titleIndex];
+            const oldTitleWords = oldTitle.split(" ");
+            const newTitleWords = newTitle.split(" ");
+            setTitleIndex(newIndex);
+            setTitle(allTitles[titleIndex]);
 
-            const inner = async () => {
-                const oldTitle = title;
-                const newTitle = allTitles[titleIndex];
-                const minLen = Math.min(oldTitle.length, newTitle.length);
-                const transitionCharecters = "@#$%&*+=<>?~";
-
-                const changesPerLetter = 2;
-                const transitionTimeMs = titleChangeDelayMs / 10; // 1/x of time is spent transitioning
-                const transitionTimePerLetterMs =
-                    transitionTimeMs / changesPerLetter / minLen;
-
-                const doSetTitle = (title: string) => {
-                    setTitle(title);
-                    console.log(title);
-                };
-
-                for (let i = 0; i < minLen; i++) {
-                    for (let j = 0; j < changesPerLetter; j++) {
-                        const getRandomChar = () => {
-                            return transitionCharecters[
-                                Math.floor(
-                                    Math.random() * transitionCharecters.length
-                                )
-                            ];
-                        };
-                        let titleFragment =
-                            newTitle.slice(0, i) +
-                            getRandomChar() +
-                            oldTitle.slice(i + 1);
-
-                        // for (let k = i; k < minLen; k++) {
-                        //     titleFragment += getRandomChar();
-                        // }
-
-                        doSetTitle(titleFragment);
-
-                        await new Promise((resolve) =>
-                            setTimeout(resolve, transitionTimePerLetterMs)
-                        );
-                        // await new Promise((resolve) => setTimeout(resolve, transitionTimePerLetterMs / changesPerLetter));
-                    }
+            // update to support different length titles
+            const newWordChanges = oldTitleWords.map((word, i) => {
+                // if new title is shorter than old title then just delete the rest of the old title
+                if (i >= newTitleWords.length) {
+                    return {
+                        steps: word.split("").map((char, i) => ({
+                            type: "deletion",
+                            oldChar: char,
+                            newChar: "",
+                            i,
+                        })),
+                        distance: word.length,
+                        matrix: [],
+                    } as LevenshteinDistanceResult;
                 }
-                doSetTitle(newTitle);
-            };
+                return levenshteinDistance(word, newTitleWords[i]);
+            });
 
-            inner().catch(console.error);
-        }, 5000);
+            // if new title is longer than old title then just insert the rest of the new title
+            if (newTitleWords.length > oldTitleWords.length) {
+                const newWords = newTitleWords.slice(oldTitleWords.length);
+                newWordChanges.push(
+                    ...newWords.map((word) => ({
+                        steps: word.split("").map((char, i) => ({
+                            type: "insertion",
+                            oldChar: "",
+                            newChar: char,
+                            i,
+                        })),
+                        distance: word.length,
+                        matrix: [],
+                    }))
+                );
+            }
+
+            setWordChanges(newWordChanges);
+        }, titleChangeDelayMs);
         return () => clearInterval(interval);
     }, [titleIndex]);
 
-    switch (varient) {
-        case 1:
+    return (
+        <>
+            <h3>Im Zach</h3>
+            <h1>
+                <span className={`${styles.title} ${styles.brandColor}`}>
+                    {(wordChanges && wordChanges.length > 0 && (
+                        <LevenshteinChangingTitle wordChanges={wordChanges} />
+                    )) ||
+                        title.split(" ").map((word, i) => (
+                            <span className={styles.word} key={i}>
+                                {word.split("").map((char, j) => (
+                                    <span className={styles.letterBase} key={j}>
+                                        {char}
+                                    </span>
+                                ))}
+                            </span>
+                        ))}
+                </span>
+            </h1>
+            <p
+                style={{
+                    maxWidth: "45ch",
+                }}
+            >
+                ... with agile software development skills and a passion for
+                creating beautiful and functional applications
+            </p>
+            <p>Hello world</p>
+            <div className={styles.callToActionButtons}>
+                <Button
+                    as={"link"}
+                    href={"/contact"}
+                    styleType={"primary"}
+                    size={"large"}
+                >
+                    Contact Me
+                </Button>
+                <Button
+                    as={"link"}
+                    href={"/about"}
+                    styleType={"secondary"}
+                    size={"large"}
+                >
+                    Learn More
+                </Button>
+            </div>
+        </>
+    );
+};
+
+const LevenshteinChangingTitle = ({
+    wordChanges,
+}: {
+    wordChanges: LevenshteinDistanceResult[];
+}) => {
+    // Helper function to generate the html for each word
+    const words = wordChanges.map((change, i) => {
+        const innerHtml = change.steps.map((step, j) => {
+            let styleClass = "";
+            switch (step.type) {
+                case "match":
+                    styleClass = styles.match;
+                    break;
+                case "substitution":
+                    styleClass = styles.substitution;
+                    break;
+                case "deletion":
+                    styleClass = styles.deletion;
+                    break;
+                case "insertion":
+                    styleClass = styles.insertion;
+                    break;
+                default:
+                    styleClass = styles.HUHHH;
+                    break;
+            }
             return (
-                <>
-                    <h1>Im Zach</h1>
-                    <h3>
-                        A <span className={styles.brandColor}>{title}</span>
-                    </h3>
-                </>
+                `<span class=${styleClass}>` +
+                `<span>${step.oldChar}</span>` +
+                `<span>${step.newChar}</span>` +
+                "</span>"
             );
-        default:
-            return (
-                <h1>
-                    Hello
-                    <br />
-                    Im Zach
-                </h1>
-            );
-    }
+        });
+
+        // we use dangerouslySetInnerHTML because without it the html
+        // animations were working inconsistently
+        return (
+            <span
+                className={styles.word}
+                key={i}
+                dangerouslySetInnerHTML={{ __html: innerHtml.join("") }}
+            />
+        );
+    });
+
+    return words;
 };
